d3339e170ddeeaad9ca043904af7109bf9c32da1
Fabio Gadau <fabiogadau92@gmail.com>
2020-06-05 13:44:04 +0200

    first setup

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php /*
+         PHP e OOP 2: Ereditarietà
+         Creare e modellizzare classi per gestire i prodotti di un magazzino
+         Creare una classe generale e poi creare altre classi che estendono questa classe generale
+         Eseguire degli output istanziando gli oggetti delle varie classi
+      */
